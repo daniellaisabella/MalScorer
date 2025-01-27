10e7a25dcea8964ca83b0a14fc4b5d367098eef8
daniellaisabella <dato0001@stud.kea.dk>
2025-01-27 14:16:57 +0100

    MatchResult Class created

---
+++ src/MatchResult.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class MatchResult {
+
+    private String teams;
+    private List<String> goalScorers;
+    public MatchResult(String teams, List<String> goalScorers) {
+//TO DO
+    }
+    public List<String> getGoalScorers() {
+//TO DO
+    }
+
+}
