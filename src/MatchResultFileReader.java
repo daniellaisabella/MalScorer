4987f071a616ec2217927616cf6d0f0435e95899
haiq0001 <haiq0001@stud.kea.dk>
2025-01-27 14:25:26 +0100

    Filereader og csv fil

---
+++ src/MatchResultFileReader.java
@@ -0,0 +1,30 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class MatchResultFileReader {
+
+
+    public MatchResultFileReader(String fileName) throws
+            FileNotFoundException {
+        // TO DO
+    }
+
+    public List<MatchResult> readFile() {
+
+        // TO DO
+        // mens der er ulæst linser i filen:
+        // indlæs en linje i filen
+        // parse dataen og opret et MatchResult objekt
+        // hvis ikke der er nogle målscorere skal listen af
+        // målscorere være tøm ellers skal listen af målscorere
+        // indholde navnet for hver mål scorede.
+    }
+}
+
+
+
+
+
+
+
+
