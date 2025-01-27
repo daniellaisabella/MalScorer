b76f72ad28c8068702e17ca27c56a7a71d84a1e6
daniellaisabella <dato0001@stud.kea.dk>
2025-01-27 14:18:39 +0100

    Statistics Class created

---
+++ src/Statistics.java
@@ -0,0 +1,20 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Statistics {
+    private List<MatchResult> matchResults;
+    public Statistics(String filename) throws FileNotFoundException {
+//TO DO
+    }
+    public Set<String> getGoalScorers() {
+//TO DO
+    }
+    public Map<String, Integer> getGoalScorersWithTotals() {
+//TO DO
+    }
+    public int getNumberOfGoals(String goalScorer) {
+//TO DO
+    }
+}
